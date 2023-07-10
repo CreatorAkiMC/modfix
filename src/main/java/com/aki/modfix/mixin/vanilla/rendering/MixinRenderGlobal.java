ff9bc90644ef892bdcf5ba525e147f7d4c1e8856
CreatorAkiMC <138881155+CreatorAkiMC@users.noreply.github.com>
2023-07-10 20:00:45 +0900

    first commit

--- src/main/java/com/aki/modfix/mixin/vanilla/rendering/MixinRenderGlobal.java
+++ src/main/java/com/aki/modfix/mixin/vanilla/rendering/MixinRenderGlobal.java
@@ -5,10 +5,7 @@ import com.aki.modfix.chunk.ChunkRenderManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.renderer.*;
-import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
-import net.minecraft.client.renderer.chunk.IRenderChunkFactory;
-import net.minecraft.client.renderer.chunk.ListChunkFactory;
-import net.minecraft.client.renderer.chunk.VboChunkFactory;
+import net.minecraft.client.renderer.chunk.*;
 import net.minecraft.client.renderer.culling.ICamera;
 import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
@@ -17,9 +14,11 @@ import net.minecraft.util.BlockRenderLayer;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
 import java.util.Set;
 
 /**
@@ -148,6 +147,72 @@ public abstract class MixinRenderGlobal {
         ChunkRenderManager.Render(blockLayerIn, partialTicks, pass, entityIn);
         cir.cancel();
     }
+
+    @Inject(method = "getRenderChunkOffset", remap = false, cancellable = true, at = @At("HEAD"))
+    public void getRenderChunkOffset(CallbackInfoReturnable<RenderChunk> info) {
+        info.setReturnValue(null);
+    }
+
+    @Redirect(method = "updateClouds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/chunk/ChunkRenderDispatcher;hasNoFreeRenderBuilders()Z"))
+    public boolean hasNoFreeRenderBuilders(ChunkRenderDispatcher chunkRenderDispatcher) {
+        return false;
+    }
+
+    /** {@link RenderGlobal#updateChunks(long)} */
+    @Inject(method = "updateChunks", cancellable = true, at = @At("HEAD"))
+    public void updateChunks(long finishTimeNano, CallbackInfo info) {
+        info.cancel();
+    }
+
+    @Inject(method = "hasNoChunkUpdates", cancellable = true, at = @At("HEAD"))
+    public void hasNoChunkUpdates(CallbackInfoReturnable<Boolean> info) {
+        info.setReturnValue(true);
+    }
+
+
+    /**
+     * renderDispatcher はtransformerのほうがいいかも
+     * */
+    /*public void setWorldAndLoadRenderers(@Nullable WorldClient worldClientIn)
+    {
+        if (this.world != null)
+        {
+            this.world.removeEventListener(this);
+        }
+
+        this.frustumUpdatePosX = Double.MIN_VALUE;
+        this.frustumUpdatePosY = Double.MIN_VALUE;
+        this.frustumUpdatePosZ = Double.MIN_VALUE;
+        this.frustumUpdatePosChunkX = Integer.MIN_VALUE;
+        this.frustumUpdatePosChunkY = Integer.MIN_VALUE;
+        this.frustumUpdatePosChunkZ = Integer.MIN_VALUE;
+        this.renderManager.setWorld(worldClientIn);
+        this.world = worldClientIn;
+
+        if (worldClientIn != null)
+        {
+            worldClientIn.addEventListener(this);
+            this.loadRenderers();
+        }
+        else
+        {
+            this.chunksToUpdate.clear();
+            this.renderInfos.clear();
+
+            if (this.viewFrustum != null)
+            {
+                this.viewFrustum.deleteGlResources();
+                this.viewFrustum = null;
+            }
+
+            if (this.renderDispatcher != null)
+            {
+                this.renderDispatcher.stopWorkerThreads();
+            }
+
+            this.renderDispatcher = null;
+        }
+    }*/
 
     /*@Unique
     public long Time = 0L;
